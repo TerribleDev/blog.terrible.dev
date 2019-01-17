4b8fec70688b0427667c6c9386e2bcb410f2dfc4
tparnell <tparnell@cargurus.com>
2019-01-16 23:40:23 -0500

    stop
    Former-commit-id: 7eb714d6d6601cad03f2e41f20645c3253b56f2d [formerly 4031c97f1237649aef5d81bd6ddbf26c5628ccd3]
    Former-commit-id: 69e35745942ae94fdbb7d9cc745942babaaf1fbe

--- TerribleDev.Blog.Web/Controllers/HomeController.cs
+++ TerribleDev.Blog.Web/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TerribleDev.Blog.Web.Models;
+using System.IO;
 
 namespace TerribleDev.Blog.Web.Controllers
 {
@@ -15,10 +16,17 @@ namespace TerribleDev.Blog.Web.Controllers
         {
             return View();
         }
-        [Route("/{*postName}")]
-        public IActionResult Post(string postName)
+        [Route("/theme/{*postName}")]
+        public IActionResult Theme(string postName)
         {
             return View(model: postName);
+        }
+        [Route("/{*postName}")]
+        public async Task<IActionResult> Post(string postName)
+        {
+            var post = await System.IO.File.ReadAllTextAsync(Path.Combine("Posts", "Writing-an-animated-flyout-hamburger-menu.md"));
+            var postRendered = Markdig.Markdown.ToHtml(post.Split("---")[1]);
+            return View(model: postRendered);
         }
         [Route("/Error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ TerribleDev.Blog.Web/Models/PostModel.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Html;
+
+namespace TerribleDev.Blog.Web.Models
+{
+    public class PostModel
+    {
+        public HtmlString Content { get; set; }
+    }
+}
