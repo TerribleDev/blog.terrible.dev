fb14bb735efa07e01e939bf84227838152c30778
tparnell <tparnell@cargurus.com>
2019-01-22 09:37:07 -0500

    cache all the things

--- src/TerribleDev.Blog.Web/Controllers/TagsController.cs
+++ src/TerribleDev.Blog.Web/Controllers/TagsController.cs
@@ -9,11 +9,13 @@ namespace TerribleDev.Blog.Web.Controllers
     public class TagsController : Controller
     {
         [Route("/all-tags")]
+        [OutputCache(Duration = 31536000)]
         public IActionResult AllTags()
         {
             return View(HomeController.tagToPost);
         }
         [Route("/tag/{tagName}")]
+        [OutputCache(Duration = 31536000, VaryByParam = "tagName")]
         public IActionResult GetTag(string tagName)
         {
             if(!HomeController.tagToPost.TryGetValue(tagName, out var models))
