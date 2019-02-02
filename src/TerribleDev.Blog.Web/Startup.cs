d873be97d882e1a39b07c097255f39979e6f882a
tparnell <tparnell@cargurus.com>
2019-02-02 14:17:20 -0500

    bettah config

--- src/TerribleDev.Blog.Web/Startup.cs
+++ src/TerribleDev.Blog.Web/Startup.cs
@@ -21,19 +21,27 @@ namespace TerribleDev.Blog.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment Env { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var blogConfig = new BlogConfiguration();
-            Configuration.GetSection("Blog").Bind(blogConfig);
-            services.AddSingleton(blogConfig);
+            Func<BlogConfiguration> getBlog = () => Configuration.GetSection("Blog").Get<BlogConfiguration>();
+            if(Env.IsDevelopment())
+            {
+                services.AddTransient(a => getBlog());
+            }
+            else 
+            {
+                services.AddSingleton(getBlog());
+            }
             services.AddResponseCompression(a =>
             {
                 a.EnableForHttps = true;
