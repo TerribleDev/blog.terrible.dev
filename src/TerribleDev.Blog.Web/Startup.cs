d846a538a0f09de410f9a7ee209143f2961c85ee
Tommy Parnell <tommy@terribledev.io>
2019-02-03 17:33:01 -0500

    headerz

--- src/TerribleDev.Blog.Web/Startup.cs
+++ src/TerribleDev.Blog.Web/Startup.cs
@@ -34,11 +34,11 @@ namespace TerribleDev.Blog.Web
         public void ConfigureServices(IServiceCollection services)
         {
             Func<BlogConfiguration> getBlog = () => Configuration.GetSection("Blog").Get<BlogConfiguration>();
-            if(Env.IsDevelopment())
+            if (Env.IsDevelopment())
             {
                 services.AddTransient(a => getBlog());
             }
-            else 
+            else
             {
                 services.AddSingleton(getBlog());
             }
@@ -70,31 +70,47 @@ namespace TerribleDev.Blog.Web
 
             app.UseHttpsRedirection();
             app.UseResponseCompression();
-            
+
             var cacheTime = env.IsDevelopment() ? 0 : 31536000;
             app.UseStaticFiles(new StaticFileOptions
             {
-                    OnPrepareResponse = ctx =>
-                    {
-                        ctx.Context.Response.Headers[HeaderNames.CacheControl] =
-                            "public,max-age=" + cacheTime;
-                    }
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers[HeaderNames.CacheControl] =
+                        "public,max-age=" + cacheTime;
+                }
             });
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img")),
-                    OnPrepareResponse = ctx =>
-                    {
-                        ctx.Context.Response.Headers[HeaderNames.CacheControl] =
-                            "public,max-age=" + cacheTime;
-                    }
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers[HeaderNames.CacheControl] =
+                        "public,max-age=" + cacheTime;
+                }
             });
             app.UseRewriter(new Microsoft.AspNetCore.Rewrite.RewriteOptions().AddRedirect("(.*[^/|.xml|.html])$", "$1/", 301));
             app.UseIENoOpen();
             app.UseNoMimeSniff();
             app.UseCrossSiteScriptingFilters();
             app.UseFrameGuard(new FrameGuardOptions(FrameGuardOptions.FrameGuard.SAMEORIGIN));
-            app.UseHsts(TimeSpan.FromDays(160), false, preload: true);
+            app.UseHsts(TimeSpan.FromDays(365), false, preload: true);
+            app.UseContentSecurityPolicy(
+                new ContentSecurityPolicy()
+                {
+                    //DefaultSrc = new HashSet<string>() {
+                    //    CSPConstants.Self, "https://www.google-analytics.com", "https://www.googletagmanager.com", "https://stats.g.doubleclick.net"
+                    //},
+                    //ScriptSrc = new HashSet<string>()
+                    //{
+                    //    CSPConstants.Self, "https://www.google-analytics.com", "https://www.googletagmanager.com", "https://stats.g.doubleclick.net"
+                    //},
+                    //StyleSrc = new HashSet<string>()
+                    //{
+                    //    CSPConstants.Self, CSPConstants.UnsafeInline
+                    //},
+                    UpgradeInsecureRequests = true
+                });
             app.UseOutputCaching();
             app.UseMvc();
         }
