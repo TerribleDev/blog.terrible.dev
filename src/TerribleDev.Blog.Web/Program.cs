16c10c9ca13c7ef9409d07f8535d418b575e7b26
Tommy Parnell <tommy@terribledev.io>
2019-02-03 22:37:57 -0500

    app insights

--- src/TerribleDev.Blog.Web/Program.cs
+++ src/TerribleDev.Blog.Web/Program.cs
@@ -19,6 +19,7 @@ namespace TerribleDev.Blog.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .ConfigureKestrel(a =>
                 {
