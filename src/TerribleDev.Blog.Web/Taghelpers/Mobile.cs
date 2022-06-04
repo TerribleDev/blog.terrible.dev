1ef4fed4f5dced7ac2461db3501f7302fcc83c4f
Tommy Parnell <tommy@terribledev.io>
2022-06-04 15:43:19 -0400

    blur initial image

--- src/TerribleDev.Blog.Web/Taghelpers/Mobile.cs
+++ src/TerribleDev.Blog.Web/Taghelpers/Mobile.cs
@@ -11,7 +11,7 @@ namespace TerribleDev.Blog.Web.Taghelpers
     [HtmlTargetElement("desktopOnly", TagStructure = TagStructure.NormalOrSelfClosing)]
     public class DesktopTagHelper : TagHelper
     {
-        static Regex MobileCheck = new Regex(@"(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino", RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Compiled);
+        static Regex MobileCheck = new Regex(@"(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino", RegexOptions.IgnoreCase | RegexOptions.ECMAScript | RegexOptions.Compiled);
         static ConcurrentDictionary<string, bool> CachedChecks = new ConcurrentDictionary<string, bool>();
         public string UserAgent { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
