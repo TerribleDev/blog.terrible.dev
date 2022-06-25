e8e9a1caa731e36ea53fafeff07faaac97da6755
Tommy Parnell <tommy@terribledev.io>
2022-06-25 10:26:41 -0400

    better etag filter

--- src/TerribleDev.Blog.Web/Filters/ETagFilter.cs
+++ src/TerribleDev.Blog.Web/Filters/ETagFilter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -11,12 +12,15 @@ namespace TerribleDev.Blog.Web.Filters
     {
         public static string staticEtag = "\"" + MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString())).ToHexString().Substring(0,8) + "\"";
         public static ConcurrentDictionary<string, string> cache = new ConcurrentDictionary<string, string>();
-        public override void OnActionExecuted(ActionExecutedContext context)
+        public override void OnActionExecuting(ActionExecutingContext context)
         {
-            if(context.HttpContext.Response.StatusCode >= 200 && context.HttpContext.Response.StatusCode < 300 && context.HttpContext.Response.Headers.ETag.Count == 0)
-            {
-                context.HttpContext.Response.Headers.Add("ETag", staticEtag);
-            }
+            context.HttpContext.Response.OnStarting(() => {
+                if(context.HttpContext.Response.StatusCode >= 200 && context.HttpContext.Response.StatusCode < 300 && context.HttpContext.Response.Headers.ETag.Count == 0)
+                {
+                    context.HttpContext.Response.Headers.Add("ETag", staticEtag);
+                }
+                return Task.CompletedTask;
+            });
         }
     }
 }
