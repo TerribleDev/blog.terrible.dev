1b372b35019d61fdc79a08cc541cd5ab34d0d64a
Tommy Parnell <tommy@terribledev.io>
2022-11-14 17:08:59 -0500

    reduce log

--- src/TerribleDev.Blog.Web/wwwroot/js/swi.js
+++ src/TerribleDev.Blog.Web/wwwroot/js/swi.js
@@ -1,13 +1,12 @@
 //Add this below content to your HTML page, or add the js file to your page at the very top to register sercie worker
 if (navigator && navigator.serviceWorker && navigator.serviceWorker.controller) {
-    console.log('[PWA Builder] active service worker found, no need to register')
 } else if (navigator && navigator.serviceWorker) {
 
     //Register the ServiceWorker
     navigator.serviceWorker.register('/sw.min.js', {
         scope: '/'
-    }).then(function (reg) {
-        console.log('Service worker has been registered for scope:' + reg.scope);
+    }).then(() => {
+        console.log('SW');
     });
 }
 
